f1d12c4f674abdc31564970c6af61d048114aac5
mayurxy <mayurxy08@gmail.com>
2018-02-04 13:41:18 -0800

    cache response complete

--- public/js/sw/index.js
+++ public/js/sw/index.js
@@ -1,19 +1,37 @@
 self.addEventListener('install', function(event) {
+  var urlsToCache = [
+    '/',
+    'js/main.js',
+    'css/main.css',
+    'imgs/icon.png',
+    'https://fonts.gstatic.com/s/roboto/v15/2UX7WLTfW3W8TclTUvlFyQ.woff',
+    'https://fonts.gstatic.com/s/roboto/v15/d-6IYplOFocCacKzxwXSOD8E0i7KZn-EPnyo3HZu7kw.woff'
+  ];
+
   event.waitUntil(
-    caches.open('wittr-static-v1').then(function(cache) {
-      return cache.addAll([
-        '/',
-        'js/main.js',
-        'css/main.css',
-        'imgs/icon.png',
-        'https://fonts.gstatic.com/s/roboto/v15/2UX7WLTfW3W8TclTUvlFyQ.woff',
-        'https://fonts.gstatic.com/s/roboto/v15/d-6IYplOFocCacKzxwXSOD8E0i7KZn-EPnyo3HZu7kw.woff'
-      ]);
-    })
+    // TODO: open a cache named 'wittr-static-v1'
+    // Add cache the urls from urlsToCache
+    cache.open('wittr-static-v1').then(function(cache){
+        return cache.addAll([
+            '/',
+            'js/main.js',
+            'css/main.css',
+            'imgs/icon.png',
+            'https://fonts.gstatic.com/s/roboto/v15/2UX7WLTfW3W8TclTUvlFyQ.woff',
+            'https://fonts.gstatic.com/s/roboto/v15/d-6IYplOFocCacKzxwXSOD8E0i7KZn-EPnyo3HZu7kw.woff'
+          ]);
+    });
   );
 });
 
 self.addEventListener('fetch', function(event) {
-  // TODO: respond with an entry from the cache if there is one.
-  // If there isn't, fetch from the network.
+  // Leave this blank for now.
+  // We'll get to this in the next task.
+    event.respondWith(
+      caches.match(event.request).then(function(response){
+        if(response) return response;
+        return fetch(event.request);
+
+      })
+    )
 });
